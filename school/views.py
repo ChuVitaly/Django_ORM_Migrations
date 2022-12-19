2dc16f59858e22df21bc4b3217b19bfc329f0a53
Виталий Чухляев <vitaliy69@mail.ru>
2022-12-19 23:55:58 +0300

    функция 'students_list()'

--- school/views.py
+++ school/views.py
@@ -14,13 +14,15 @@ from .models import Student
 def students_list(request):
     object_list = Student.objects.all()
     template = 'school/students_list.html'
+    # students = Student.object.filter(students=teacher).order_by('teachers')
     context = {
-        'header': 'Список студентов',
-        'object_list': object_list
+        'header_students': 'Список студентов',
+        'header_teachers': 'Список учителей студентов',
+        'object_list': object_list,
+        # 'student': students
     }
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    # ordering = 'group'
-
+#     # используйте этот параметр для упорядочивания результатов
+#     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+#     # ordering = 'group'
+#
     return render(request, template, context)
